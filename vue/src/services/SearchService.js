38e55e42b35d8e13eb9c4c7df466a67c011ee6d0
Erin Brau <erin.brau@gmail.com>
2021-08-02 17:45:17 -0400

    Built out Home Search Form and SearchService

---
+++ vue/src/services/SearchService.js
@@ -0,0 +1,15 @@
+// import axios from 'axios';
+
+// const http = axios.create({
+//   baseURL: "http://localhost:8080"
+// });
+
+export default {
+
+  getSearchResults(searchTerm) {
+    //TODO: change path to match the back-end controller 
+    //return http.get('/boards');
+    console.log(searchTerm);
+  }
+
+}
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -2,16 +2,41 @@
   <div class="home">
     
     <h1>WLK - PHL</h1>
-    <p>You must be authenticated to see this</p>
+    <p>Fun Stuff Near You</p>
     <!-- <router-link :to= " { name: 'login'} "><button>Login</button></router-link> -->
+    <div class="search-box">
+      <form v-on:submit.prevent="submitSearch">
+        <input type="text" placeholder="What do you want to do?" v-model="searchTerm.text">
+        <button type="submit">Search</button>
+      </form>
+    </div>
+    <div class="categories">
+
+    </div>
   </div>
 </template>
 
 <script>
-
+import searchService from "../services/SearchService";
 
 export default {
-
-  name: "home"
+  name: "home",
+  data() {
+    return {
+      searchTerm: {
+        text: ""
+      }
+    };
+  },
+  methods: {
+    resetForm(){
+      this.searchTerm = {};
+    },
+    submitSearch(searchTerm){
+      searchService.getSearchResults(searchTerm);
+      //TODO: change to search page results view/vue when built
+      this.$router.push({name: "login"});
+    }
+  }
 };
 </script>
